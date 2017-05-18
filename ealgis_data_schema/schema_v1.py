cbcd747d2efe6d4bf4a32e81a572b1e447294797
Grahame Bowland <grahame@angrygoats.net>
2017-05-19 00:58:56 +0800

    wip schema

--- ealgis_data_schema/schema_v1.py
+++ ealgis_data_schema/schema_v1.py
@@ -1,84 +1,112 @@
 import sqlalchemy as db
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from sqlalchemy.orm import relationship, backref
 import datetime
 
-Base = declarative_base()
 
-
-class TableInfo(Base):
+class TableInfo(object):
     "metadata for each table that has been loaded into the system"
     __tablename__ = 'table_info'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), unique=True, index=True)
-    geometry_source = relationship(
-        'GeometrySource',
-        backref=backref('table_info'),
-        cascade="all",
-        uselist=False)
-    column_info = relationship(
-        'ColumnInfo',
-        backref=backref('table_info'),
-        cascade="all",
-        lazy='dynamic')
-    linkages = relationship(
-        'GeometryLinkage',
-        backref=backref('attribute_table'),
-        cascade="all",
-        lazy='dynamic')
     metadata_json = db.Column(db.String(2048))
 
+    @declared_attr
+    def geometry_source(cls):
+        return relationship(
+            'GeometrySource',
+            backref=backref('table_info'),
+            cascade="all",
+            uselist=False)
 
-class ColumnInfo(Base):
+    @declared_attr
+    def column_info(cls):
+        return relationship(
+            'ColumnInfo',
+            backref=backref('table_info'),
+            cascade="all",
+            lazy='dynamic')
+
+    @declared_attr
+    def linkages(cls):
+        return relationship(
+            'GeometryLinkage',
+            backref=backref('attribute_table'),
+            cascade="all",
+            lazy='dynamic')
+
+
+class ColumnInfo(object):
     "metadata for columns in the tabbles"
     __tablename__ = 'column_info'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True)
-    tableinfo_id = db.Column(db.Integer, db.ForeignKey('table_info.id'), index=True, nullable=False)
     metadata_json = db.Column(db.String(2048))
     __table_args__ = (db.UniqueConstraint('name', 'tableinfo_id'), )
 
+    @declared_attr
+    def tableinfo_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('table_info.id'), index=True, nullable=False)
 
-class GeometrySourceProjected(Base):
+
+class GeometrySourceProjected(object):
     "details of an additional column (on the same table as the source) with the source reprojected to this srid"
     __tablename__ = 'geometry_source_projected'
     id = db.Column(db.Integer, primary_key=True)
-    geometry_source_id = db.Column(db.Integer, db.ForeignKey('geometry_source.id'), index=True, nullable=False)
     srid = db.Column(db.Integer, nullable=False)
     column = db.Column(db.String(256), nullable=False)
 
+    @declared_attr
+    def geometry_source_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('geometry_source.id'), index=True, nullable=False)
 
-class GeometrySource(Base):
+
+class GeometrySource(object):
     "table describing sources of geometry information: the table, and the column"
     __tablename__ = 'geometry_source'
     id = db.Column(db.Integer, primary_key=True)
-    tableinfo_id = db.Column(db.Integer, db.ForeignKey('table_info.id'), index=True, nullable=False)
     geometry_type = db.Column(db.String(256), nullable=False)
     column = db.Column(db.String(256), nullable=False)
     srid = db.Column(db.Integer, nullable=False)
     gid = db.Column(db.String(256), nullable=False)
-    linkages = relationship(
-        'GeometryLinkage',
-        backref=backref('geometry_source'),
-        cascade="all",
-        lazy='dynamic')
-    reprojections = relationship(
-        'GeometrySourceProjected',
-        backref=backref('geometry_source'),
-        cascade="all",
-        lazy='dynamic')
-    from_relations = relationship(
-        'GeometryRelation',
-        backref=backref('geometry_source'),
-        cascade="all",
-        lazy='dynamic',
-        foreign_keys="[GeometryRelation.geo_source_id]")
-    with_relations = relationship(
-        'GeometryRelation',
-        backref=backref('overlaps_geometry_source'),
-        cascade="all",
-        lazy='dynamic',
-        foreign_keys="[GeometryRelation.overlaps_with_id]")
+
+    @declared_attr
+    def tableinfo_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('table_info.id'), index=True, nullable=False)
+
+    @declared_attr
+    def linkages(cls):
+        return relationship(
+            'GeometryLinkage',
+            backref=backref('geometry_source'),
+            cascade="all",
+            lazy='dynamic')
+
+    @declared_attr
+    def reprojections(cls):
+        return relationship(
+            'GeometrySourceProjected',
+            backref=backref('geometry_source'),
+            cascade="all",
+            lazy='dynamic')
+
+    @declared_attr
+    def from_relations(cls):
+        return relationship(
+            'GeometryRelation',
+            backref=backref('geometry_source'),
+            cascade="all",
+            lazy='dynamic',
+            foreign_keys="[GeometryRelation.geo_source_id]")
+
+    @declared_attr
+    def with_relations(cls):
+        return relationship(
+            'GeometryRelation',
+            backref=backref('overlaps_geometry_source'),
+            cascade="all",
+            lazy='dynamic',
+            foreign_keys="[GeometryRelation.overlaps_with_id]")
 
     def __repr__(self):
         return "GeometrySource<%s.%s>" % (self.table_info.name, self.column)
@@ -93,29 +121,43 @@ class GeometrySource(Base):
             return None
 
 
-class GeometryLinkage(Base):
+class GeometryLinkage(object):
     "details of links to tie attribute data to columns in a geometry table"
     __tablename__ = 'geometry_linkage'
     id = db.Column(db.Integer, primary_key=True)
-    # the geometry table, and the column which links a row in our attribute table with
-    # a row in the geometry table
-    geo_source_id = db.Column(db.Integer, db.ForeignKey('geometry_source.id'), index=True, nullable=False)
     geo_column = db.Column(db.String(256))
-    # the attribute table, and the column which links a row in our geomtry table with
-    # a row in the attribute table
-    attr_table_info_id = db.Column(db.Integer, db.ForeignKey('table_info.id'), nullable=False)
+
+    @declared_attr
+    def attr_table_info_id(cls):
+        """
+        the attribute table, and the column which links a row in our geomtry table with
+        a row in the attribute table
+        """
+        return db.Column(db.Integer, db.ForeignKey('table_info.id'), nullable=False)
+
+    @declared_attr
+    def geo_source_id(cls):
+        """
+        the geometry table, and the column which links a row in our attribute table with
+        a row in the geometry table
+        """
+        return db.Column(db.Integer, db.ForeignKey('geometry_source.id'), index=True, nullable=False)
+
     attr_column = db.Column(db.String(256))
 
 
-class GeometryIntersection(Base):
+class GeometryIntersection(object):
     "intersection of two priority geometries, indexed by gids from their source columns"
     __tablename__ = 'geometry_intersection'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    geometry_relation_id = db.Column(db.Integer, db.ForeignKey('geometry_relation.id'), index=True, nullable=False)
     gid = db.Column(db.Integer, index=True, nullable=False)
     with_gid = db.Column(db.Integer, index=True, nullable=False)
     percentage_overlap = db.Column(db.Float, nullable=False)
     area_overlap = db.Column(db.Float, nullable=False)
+
+    @declared_attr
+    def geometry_relation_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('geometry_relation.id'), index=True, nullable=False)
 
     def __repr__(self):
         return "<%d,%d>: %.2f%%" % (
@@ -124,13 +166,16 @@ class GeometryIntersection(Base):
             self.percentage_overlap)
 
 
-class GeometryTouches(Base):
+class GeometryTouches(object):
     "geometries that touch but do not overlap"
     __tablename__ = 'geometry_touches'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    geometry_relation_id = db.Column(db.Integer, db.ForeignKey('geometry_relation.id'), index=True, nullable=False)
     gid = db.Column(db.Integer, index=True, nullable=False)
     with_gid = db.Column(db.Integer, index=True, nullable=False)
+
+    @declared_attr
+    def geometry_relation_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('geometry_relation.id'), index=True, nullable=False)
 
     def __repr__(self):
         return "<%d,%d>" % (
@@ -138,28 +183,41 @@ class GeometryTouches(Base):
             self.with_gid)
 
 
-class GeometryRelation(Base):
+class GeometryRelation(object):
     "relationship of geometries; eg. one row in a geometry table with another row in another table (possibly the same table)"
     __tablename__ = 'geometry_relation'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    geo_source_id = db.Column(db.Integer, db.ForeignKey('geometry_source.id'), nullable=False)
-    overlaps_with_id = db.Column(db.Integer, db.ForeignKey('geometry_source.id'), nullable=False)
-    intersections = relationship(
-        'GeometryIntersection',
-        cascade="all",
-        backref=backref('relation'),
-        lazy='dynamic')
-    touches = relationship(
-        'GeometryTouches',
-        cascade="all",
-        backref=backref('relation'),
-        lazy='dynamic')
-    __table_args__ = (
-        db.UniqueConstraint('geo_source_id', 'overlaps_with_id'),
-        db.Index('georelation_lookup', geo_source_id, overlaps_with_id))
+
+    @declared_attr
+    def geo_source_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('geometry_source.id'), nullable=False)
+
+    @declared_attr
+    def overlaps_with_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('geometry_source.id'), nullable=False)
+
+    @declared_attr
+    def intersections(cls):
+        return relationship(
+            'GeometryIntersection',
+            cascade="all",
+            backref=backref('relation'),
+            lazy='dynamic')
+
+    @declared_attr
+    def touches(cls):
+        return relationship(
+            'GeometryTouches',
+            cascade="all",
+            backref=backref('relation'),
+            lazy='dynamic')
+
+    __table_args__ = (db.UniqueConstraint('geo_source_id', 'overlaps_with_id'),)
+    # FIXME: add the index
+    # db.Index('georelation_lookup', geo_source_id, overlaps_with_id))
 
 
-class EALGISMetadata(Base):
+class EALGISMetadata(object):
     "metadata table for a given set of datasets in a schema"
     __tablename__ = 'ealgis_metadata'
     id = db.Column(db.Integer, primary_key=True)
@@ -167,3 +225,40 @@ class EALGISMetadata(Base):
     version = db.Column(db.Float())
     description = db.Column(db.Text())
     date = db.Column(db.DateTime(timezone=True), default=datetime.datetime.utcnow)
+
+
+_schemas = {}
+
+
+class SchemaStore:
+    _schema_classes = [
+        EALGISMetadata,
+        TableInfo,
+        # ColumnInfo,
+        # GeometrySourceProjected,
+        # GeometrySource,
+        # GeometryLinkage,
+        # GeometryIntersection,
+        # GeometryTouches,
+        # GeometryRelation,
+    ]
+
+    def __init__(self):
+        self.bases = {}
+        self.classes = {}
+
+    def _import_schema(self, schema_name):
+        Base = self.bases[schema_name] = declarative_base()
+        self.classes[schema_name] = classes = {}
+        for cls in self._schema_classes:
+            print("register class:", cls)
+            classes[cls.__name__] = type(schema_name.replace('_', '').upper() + cls.__name__, (cls, Base), {'__table_args__': {'schema': schema_name}})
+
+    def load_schema(self, schema_name):
+        if schema_name not in self.classes:
+            self._import_schema(schema_name)
+        print(self.classes, self.bases)
+        return self.bases[schema_name], self.classes[schema_name]
+
+
+store = SchemaStore()
