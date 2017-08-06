cc10d36651e7984779c0b963d4df0e9bee0f9349
Grahame Bowland <grahame@angrygoats.net>
2017-08-06 23:55:34 +0800

    schema

--- ealgis_data_schema/schema_v1.py
+++ ealgis_data_schema/schema_v1.py
@@ -1,18 +1,16 @@
-import sqlalchemy as db
-from sqlalchemy.schema import Table, Column, MetaData, ForeignKey
-from sqlalchemy.types import JSON, Integer, String
+from sqlalchemy.schema import (
+    Table,
+    Column,
+    MetaData,
+    ForeignKey)
+from sqlalchemy.types import (
+    Text,
+    JSON,
+    DateTime,
+    Integer,
+    String)
 from collections import defaultdict
 import datetime
-
-
-class EALGISMetadata(object):
-    "metadata table for a given set of datasets in a schema"
-    __tablename__ = 'ealgis_metadata'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
-    version = db.Column(db.Float())
-    description = db.Column(db.Text())
-    date = db.Column(db.DateTime(timezone=True), default=datetime.datetime.utcnow)
 
 
 class SchemaStore:
@@ -26,6 +24,14 @@ class SchemaStore:
 
         metadata = self.metadata[schema_name]
         tables = self.tables[schema_name]
+        tables.append(Table(
+            "ealgis_metadata", metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(256)),
+            Column('version', String(256)),
+            Column('description', Text()),
+            Column('date_created', DateTime(timezone=True), default=datetime.datetime.utcnow),
+            schema=schema_name))
         tables.append(Table(
             "table_info", metadata,
             Column('id', Integer, primary_key=True),
@@ -42,14 +48,15 @@ class SchemaStore:
         tables.append(Table(
             "geometry_source", metadata,
             Column('id', Integer, primary_key=True),
+            Column('gid', String(256)),
             Column('table_info_id', Integer, fkey('table_info.id'), nullable=False),
+            Column('geometry_type', String(256)),
             schema=schema_name))
         tables.append(Table(
             "geometry_source_column", metadata,
             Column('id', Integer, primary_key=True),
             Column('geometry_source_id', Integer, fkey('geometry_source.id'), nullable=False),
             Column('column', String(256)),
-            Column('srid', String(256)),
             Column('srid', Integer),
             schema=schema_name))
         tables.append(Table(
@@ -59,6 +66,13 @@ class SchemaStore:
             Column('geometry_source_id', Integer, fkey('geometry_source.id'), nullable=False),
             Column('geo_column', String(256)),
             Column('attr_column', String(256)),
+            schema=schema_name))
+        tables.append(Table(
+            "mailbox", metadata,
+            Column('id', Integer, primary_key=True),
+            Column('from', String(256)),
+            Column('to', String(256)),
+            Column('message', JSON()),
             schema=schema_name))
 
     def load_schema(self, schema_name):
