cb205afd3b1afbcbfaa3b2cfbb1b18bbfc3c2ac1
Grahame Bowland <grahame@oreamnos.com.au>
2019-08-25 22:00:05 +0800

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-install_requires = ["SQLAlchemy==1.1.9"]
+install_requires = ["SQLAlchemy==1.3.7"]
 
 setup(
     author="Grahame Bowland",
-    author_email="grahame@angrygoats.net",
+    author_email="grahame@oreamnos.com.au",
     description="EAlGIS data schema",
     long_description="EAlGIS data schema",
     license="GPL3",
     keywords="ealgis",
     url="https://github.com/ealgis/ealgis-data-schema",
     name="ealgis_data_schema",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=install_requires,
 )
